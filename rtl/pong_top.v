2ba7f2d2aead492724f6bc82cbb6f239bab88cab
subhan-razzaq <subhanrazzaq777@gmail.com>
2026-09-23 12:10:28 -0400

    Generated Bitstream and Tested Seven Segment Functionality and Added Reports for this Test

--- rtl/pong_top.v
+++ rtl/pong_top.v
@@ -1,11 +1,16 @@
 `timescale 1 ns / 1 ps
-module pong_top(input wire clk, output wire Hsync, Vsync, output wire [3:0] vgaRed, vgaGreen, vgaBlue);
+module pong_top(input wire clk, input wire [15:0] sw, output wire Hsync, Vsync, dp, output wire [3:0] vgaRed, vgaGreen, vgaBlue, an, output wire [6:0] seg);
     wire clk_25;
+    wire clk_1k;
     wire o_hsync, o_vsync;
     wire o_hsync_tp, o_vsync_tp;
-    wire [3:0] o_red_tp, o_green_tp, o_blue_tp;
-    clk_en #(.DIV(4)) inst_clk(.clk(clk), .o_ce(clk_25));
+    wire [3:0] blank, o_red_tp, o_green_tp, o_blue_tp;
+    assign dp = 1'b1;
+    assign blank = 4'b0000;
+    clk_en #(.DIV(4)) inst_clk0(.clk(clk), .o_ce(clk_25));
+    clk_en #(.DIV(100_000)) inst_clk1(.clk(clk), .o_ce(clk_1k));
     vga_sync_pulses inst_pulses(.clk(clk), .i_ce(clk_25), .o_hsync(o_hsync), .o_vsync(o_vsync));
     test_pattern inst_test_pattern(.clk(clk), .i_ce(clk_25), .i_hsync(o_hsync), .i_vsync(o_vsync), .o_hsync(o_hsync_tp), .o_vsync(o_vsync_tp), .o_red(o_red_tp), .o_green(o_green_tp), .o_blue(o_blue_tp));
     vga_sync_porch inst_porch(.clk(clk), .i_ce(clk_25), .i_hsync(o_hsync_tp), .i_vsync(o_vsync_tp), .i_red(o_red_tp), .i_green(o_green_tp), .i_blue(o_blue_tp), .o_hsync(Hsync), .o_vsync(Vsync), .o_red(vgaRed), .o_green(vgaGreen), .o_blue(vgaBlue));
+    seven_seg_mux inst_seven_seg(.clk(clk), .i_ce(clk_1k), .i_digits(sw), .i_blank(blank), .o_seg(seg), .o_an(an)); // switches used as digits just for testing purposes now.
 endmodule
